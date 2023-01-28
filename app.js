1fd00104ee195518ce65ecc1be38061acde58600
abdul qadir <aqmemon54@gmail.com>
2023-01-28 22:42:01 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// var age = +prompt("enter your age");
+// if(age == 20){
+//     alert("your age is equal")
+// }
+// else if (age < 20){
+//     alert("your age is less")
+// }
+// else{
+//    alert("your age is more")
+// }
+
+// var percent = +prompt ("enter your pacentage")
+// if(percent >= 80 && percent <= 90){
+//     alert("A+")
+// }
+// else if(percent >= 70 && percent <= 80){
+//     alert("A")
+// }
+// else if(percent >= 60 && percent <= 70){
+//     alert("B")
+// }
+// else if(percent >= 50 && percent <= 60){
+//     alert("C")
+// }
+// else if(percent >= 40 && percent <= 50){
+//     alert("D")
+
+// }
+// else if(percent >= 30 && percent <= 40){
+//     alert("E")
+// }
+// else if(percent >= 0 && percent <= 30){
+//     alert("Fail")
+// }
+// else{
+//     alert("you didnot correct percentage")
+// }
